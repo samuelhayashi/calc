04a4089d3f9f60e617f71a3af4356a4e67abef16
Samuel Hayashi <samuel@shayashi.net>
2014-05-06 20:33:12 +0900

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+// Required packages
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var compass = require('gulp-compass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// SASS/Compass
+gulp.task('compass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(compass({
+            config_file: './config.rb',
+            css: 'css',
+            sass: 'scss'
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+// Autoprefix & Minify CSS
+gulp.task('css', function() {
+    return gulp.src(['css/normalize.css', 'css/style.css'])
+        .pipe(autoprefixer('last 1 version'))
+        .pipe(concat('main.css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('css/min'));
+});
+
+// Concatenate & Minify JS Files
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+      .pipe(concat('main.js'))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(uglify())
+      .pipe(gulp.dest('js/min'));
+});
+
+// Concatenate & Minify JS Files
+gulp.task('vendor-update', function() {
+    return gulp.src(['bower_components/html5-boilerplate/js/vendor/*','bower_components/respond/dest/respond.min.js'])
+      .pipe(gulp.dest('js/vendor'));
+});
+
+// Watch
+gulp.task('watch', function() {
+    gulp.watch('scss/*.scss', ['compass']);
+    gulp.watch('css/*.css', ['css']);
+    gulp.watch('js/*.js', ['scripts']);
+});
+
+// Define default tasks
+gulp.task('default', ['compass','css','scripts','watch']);
